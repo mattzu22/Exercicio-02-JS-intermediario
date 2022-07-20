97f8a0909a7b6431704f5510efdd652bdd326fd5
Matheus Macedo <matheusg3.201168@gmail.com>
2022-07-20 02:34:07 -0300

    Exercicio finalizado

--- src/js/index.js
+++ src/js/index.js
@@ -1,97 +1,18 @@
-/*
-    1- capturar as perguntas
-    2- capturar a info
-*/
 
-const perguntas = document.getElementsByClassName('pergunta')
+const perguntas = document.querySelectorAll(".pergunta")
 
-let pergunta = document.querySelectorAll(".pergunta")
+perguntas.forEach(pergunta => {
 
-let informacoes = document.getElementsByClassName('info')
+    pergunta.addEventListener('click',  () => {
 
-const setaAtivada = document.getElementsByClassName('seta')
-const escondido = document.querySelector('.escondido')
+        const perguntaAtivaAtual = document.querySelector('.ativo')
 
+        if(perguntaAtivaAtual){
+            perguntaAtivaAtual.classList.remove('ativo')
+        }
 
-    pergunta.forEach(pergunta =>{
-
-        pergunta.addEventListener('click', () =>{
-
-            const idDaPerguntaClicada = pergunta.attributes.id.value
-            
-            const idDaRespostaParaAbrir = idDaPerguntaClicada + '-pergunta'
-            
-            const respostaParaAbrir = document.getElementById(idDaRespostaParaAbrir)
-
-            respostaParaAbrir.classList.remove('escondido')
-
-            console.log(informacoes != escondido)
-
-            if(informacoes != escondido){
-                informacoes.classList.add('escondido')
-            }
-         
-        })
+        pergunta.classList.add('ativo')
 
     })
 
-   
-
-
-
-// pergunta[0].addEventListener('click', () => {
-
-
-//     informacoes[0].style.display = 'block'
-//     setaAtivada[0].style.filter = 'opacity(0.5) drop-shadow(0 0 0 orange) contrast(2)'
-//     setaAtivada[0].style.transform = 'rotate(0)'
-
-
-//     setaAtivada[1].style.transform = 'rotate(-90deg)'
-//     setaAtivada[2].style.transform = 'rotate(-90deg)'
-
-//     setaAtivada[1].style.filter = 'none'
-//     setaAtivada[2].style.filter = 'none'
-
-//     informacoes[1].style.display = 'none'
-//     informacoes[2].style.display = 'none'
-
-// })
-
-// pergunta[1].addEventListener('click', () => {
-
-
-//     informacoes[1].style.display = 'block'
-//     setaAtivada[1].style.filter = 'opacity(0.5) drop-shadow(0 0 0 orange) contrast(2)'
-//     setaAtivada[1].style.transform = 'rotate(0)'
-
-//     setaAtivada[0].style.transform = 'rotate(-90deg)'
-//     setaAtivada[2].style.transform = 'rotate(-90deg)'
-
-//     setaAtivada[0].style.filter = 'none'
-//     setaAtivada[2].style.filter = 'none'
-
-//     informacoes[0].style.display = 'none'
-//     informacoes[2].style.display = 'none'
-
-// })
-
-
-// pergunta[2].addEventListener('click', () => {
-
-
-//     informacoes[2].style.display = 'block'
-//     setaAtivada[2].style.filter = 'opacity(0.5) drop-shadow(0 0 0 orange) contrast(2)'
-//     setaAtivada[2].style.transform = 'rotate(0)'
-
-
-//     setaAtivada[0].style.transform = 'rotate(-90deg)'
-//     setaAtivada[1].style.transform = 'rotate(-90deg)'
-
-//     setaAtivada[1].style.filter = 'none'
-//     setaAtivada[0].style.filter = 'none'
-
-//     informacoes[1].style.display = 'none'
-//     informacoes[0].style.display = 'none'
-
-// })
+})
